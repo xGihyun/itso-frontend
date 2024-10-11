3e2ca637a8af220469b736636c54637954a86c9e
xGihyun <giordnnuz@gmail.com>
2024-10-11 20:03:58 +0800

    Finished about page

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -3,5 +3,19 @@ import itsoLogo from "@/assets/images/itso-logo.png";
 import ccisLogo from "@/assets/images/ccis-logo.png";
 import itsoTorch from "@/assets/images/itso-torch.png";
 import itsoBlueBg from "@/assets/images/itso-blue-bg.png";
+import itsoRobot from "@/assets/images/itso-robot.jpg";
+import ccisWuriCertificate from "@/assets/images/ccis-wuri-certificate.jpg";
+import wuriLogo from "@/assets/images/wuri-logo.webp";
+import chedLogo from "@/assets/images/ched-logo.png";
 
-export { itsoUmakLogo, ccisLogo, itsoTorch, itsoLogo, itsoBlueBg };
+export {
+  itsoUmakLogo,
+  ccisLogo,
+  itsoTorch,
+  itsoLogo,
+  itsoBlueBg,
+  itsoRobot,
+  ccisWuriCertificate,
+  wuriLogo,
+  chedLogo,
+};
